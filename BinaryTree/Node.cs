004f4b3e48ecdc268004559945d1f0d285ff3801
Rakesh <rbaruah@gmail.com>
2018-05-23 09:00:40 -0500

    first commit

---
+++ BinaryTree/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        //member variables
+        public int value;
+        public Node left;
+        public Node right;
+        
+
+        //constructor
+
+        //member methods
+        public void Traverse(Node startingNode)
+        {
+            //move to the left child node
+            if (startingNode.left != null)
+            {
+                myNode = startingNode.left;
+            }
+
+            //move to the right child node
+            if (startingNode.right != null)
+            {
+                myNode = startingNode.right;
+            }
+            
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Tree
+    {
+
+        //member variables
+        public Node root;
+
+        //constructor
+
+        //member methods
+        public void Add(int value)
+        {
+            if (root == null)
+            {
+                root.value = value;
+            }
+            else if (value < root.value)
+            {
+                //move to root.left node
+                root.Traverse(root.left);
+                //check the value of root.left node
+                if (root.left == null)
+                //if root.left == null, THEN root.left.value = value;
+                {
+                    root.left.value = value;
+                    Node newNode = new Node();
+                    newNode = root.left;
+                }
+                //if root.left != null, THEN check the value of root.left
+                else if (value < root.left.value)
+                {
+                    root.left.Traverse(root.left.left);
+
+                }
+                //if value < root.left, THEN move to the left child node of root.left
+                else
+                {
+                    root.left.Traverse(root.left.right);
+                }
+            }
+            else if (value > root.value)
+            {
+                //move to root.right node
+                root.Traverse(root.right);
+                //check the value of root.right node
+                if (root.right == null)
+                //if root.right == null, THEN root.right.value = value;
+                {
+                    root.right.value = value;
+                    Node newNode = new Node();
+                    newNode = root.right;
+                }
+                else if (value < root.right.value)
+                {
+                    root.right.Traverse(root.right.left);
+
+                }
+                else
+                {
+                    root.right.Traverse(root.right.right);
+                }
+                
+                //if root.right != null, THEN check the value of root.right;
+                //if value > root.right, THEN move to the right child of node.right
+            }
+
+        }
+
+        public void Search(int value)
+        {
+
+        }
+    }
+}
